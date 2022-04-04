1385d0047a3778aa9a4020c7e9c73c5ed9bab885
hsritz2009 <98743112+hsritz2009@users.noreply.github.com>
2022-04-04 11:29:43 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 4) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
 })
 basic.showString("hola")
 music.playMelody("C5 G A G D F G C ", 120)
